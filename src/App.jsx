2c7adb86d8aad211b34fef9e6684099dadadaf3f
Harmon Rod Manogura <harmonmanogura@umpisa.co>
2024-02-25 12:39:01 +0800

    [FEAT] font change and bg

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,16 @@ function App() {
       <Couple />
       <Couple />
     </div>
-      <div>
+      <div >
         <TimeCountdown />
       </div>
       <div className='mt-10 w-full'>
         <Hyperlinks links={[
-          { icon: 'logo/1.png', title: 'RSVP', url: '#' },
+          { icon: 'logo/1.png', title: 'RSVP', url: 'https://forms.gle/ANzMX3LEJFfmikWR7' },
           { icon: 'logo/2.png', title: 'PRE-WEDDING FILM', url: '#' },
-          { icon: 'logo/3.png', title: 'E-INVITE', url: '#' },
+          { icon: 'logo/3.png', title: 'E-INVITE', url: 'https://www.canva.com/design/DAF8-pUHk_M/DMG30qGPFaPdTAphj1tQYQ/watch?utm_content=DAF8-pUHk_M&utm_campaign=designshare&utm_medium=link&utm_source=editor' },
           { icon: 'logo/4.png', title: 'COME FOR THE LOVE', url: 'https://maps.app.goo.gl/e44z82twTh1R3CG2A' },
-          { icon: 'logo/5.png', title: 'WEDDING FAQs', url: '#' },
+          { icon: 'logo/5.png', title: 'WEDDING FAQs', url: 'https://www.canva.com/design/DAF9w8OvORA/oXbv2cPLEStLw9rBjNxXog/view?utm_content=DAF9w8OvORA&utm_campaign=designshare&utm_medium=link&utm_source=editor' },
           { icon: 'logo/6.png', title: 'ATTIRE GUIDE', url: '#' },
         ]} />
       </div>
