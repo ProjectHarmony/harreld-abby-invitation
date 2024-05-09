30d37bce13cb8dbb36f68aa6744d4b2b305a7b54
Harmon Rod Manogura <harmonmanogura@umpisa.co>
2024-05-09 20:25:55 +0800

    [FINAL] push for client

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import Hyperlinks from './sections/hyperlinks';
 import CountdownTimer from './sections/countdown-timer';
 import BGM from './sections/bgm';
-import Header from './sections/header';
+import InvitePdf from './invite.pdf';
+
+
 
 function App() {
+  
   return (
     <div className="flex flex-col items-center relative min-h-screen overflow-auto" style={{
       backgroundImage: `url('/bg-brown.jpg')`,
@@ -12,13 +15,13 @@ function App() {
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
     }}>
-      <div style={{ height: '7.5rem' }}></div>
+      <div style={{ height: '7rem' }}></div>
 
       <p className='text-center greatvibes text-5xl mt-10 text-[#362917] font-extrabold '>
         Harreld & Abegail
       </p>
       <div style={{ height: '1rem' }}></div>
-      <p className='text-center tracking-wider mt-2 mb-1 greatvibes text-3xl text-[#362917]'>
+      <p className='text-center tracking-wider mt-2 mb-1 font-extrabold greatvibes text-3xl text-[#362917]'>
         we invite you to join our wedding
       </p>
 
@@ -29,11 +32,10 @@ function App() {
       <div className='mt-10 px-3'>
         <Hyperlinks links={[
           { title: 'RSVP', url: 'https://forms.gle/ANzMX3LEJFfmikWR7' },
-          { title: 'E-INVITE', url: 'https://www.canva.com/design/DAGEu_t6avY/usqEr6gRGJ-PH778qWUXgw/view?utm_content=DAGEu_t6avY&utm_campaign=designshare&utm_medium=link&utm_source=editor' },
+          { title: 'E-INVITE', url: InvitePdf, target: '_blank' },
           { title: 'COME FOR THE LOVE', url: 'https://maps.app.goo.gl/NwWnYf3qVfduZc24A' },
           { title: 'RECEPTION VENUE', url: 'https://maps.app.goo.gl/wGoPDdoE3N8QkrPZ7' },
           { title: 'WEDDING FAQs', url: 'https://www.canva.com/design/DAF9w8OvORA/oXbv2cPLEStLw9rBjNxXog/view?utm_content=DAF9w8OvORA&utm_campaign=designshare&utm_medium=link&utm_source=editor' },
-          { title: 'ATTIRE GUIDE', url: 'https://www.canva.com/design/DAGEuzzC-yY/23WhEPKcEZX2rGVdBWgmHw/view?utm_content=DAGEuzzC-yY&utm_campaign=designshare&utm_medium=link&utm_source=editor' },
         ]} />
       </div>
       <div className='mt-10 mb-12 fixed bottom-0 left-1/2 transform -translate-x-1/2 z-50 w-[80%]'>
--- src/sections/hyperlinks.jsx
+++ src/sections/hyperlinks.jsx
@@ -4,12 +4,12 @@ const Hyperlinks = ({ links }) => {
     return (
         <div className='flex flex-col m-0 w-full border-t-1 border-b-1 px-5'>
             {links.map((link, index) => (
-                <a key={index} href={link.url} className="flex px-6 py-2 leading-6 
-                    text-center text-[#362917] arimo uppercase transition rounded hover:shadow-lg 
-                    hover:bg-[#818076]  focus:outline-none items-center justify-center 
-                    w-full sm:w-auto"
-                    style={{ fontSize: '1.6rem' }}>
+                <a key={index} href={link.url} target={link.target}
+                    rel="noopener noreferrer" className="flex px-6 py-2 leading-6 
+        text-center text-[#362917] arimo uppercase transition rounded hover:shadow-lg 
+        hover:bg-[#818076] focus:outline-none items-center justify-center" style={{ fontSize: '1.6rem' }}>
                     {link.title}
+
                 </a>
             ))}
         </div>
